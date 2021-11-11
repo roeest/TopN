9a308cc41d88ee967f1953a6e2c30200c41c61c3
roeest <starkroee@gmail.com>
2021-11-11 15:23:49 -0600

    get weights from vanguard api

--- go.mod
+++ go.mod
@@ -2,6 +2,5 @@ module fin
 
 go 1.15
 
-require github.com/roeest/vanguard v0.0.0-20180826174649-559d97c03b80
+require github.com/roeest/vanguard v0.0.1
 
-replace github.com/roeest/vanguard => c:\Users\roee\documents\projects\vanguard
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"flag"
 	"fmt"
 	"log"
@@ -10,32 +11,44 @@ import (
 )
 
 type sectorDetails struct {
-	weight      float64
 	description string
 }
 
-var sectors = map[string]sectorDetails{
-	"VOX": {weight: 0.1, description: "Communication Services"},
-	"VCR": {weight: 0.102, description: "Consumer Discretionary"},
-	"VDC": {weight: 0.067, description: "Consumer Staples"},
-	"VDE": {weight: 0.06, description: "Energy"},
-	"VFH": {weight: 0.137, description: "Financials"},
-	"VHT": {weight: 0.149, description: "Health Care"},
-	"VIS": {weight: 0.097, description: "Industrials"},
-	"VGT": {weight: 0.208, description: "Information Technology"},
-	"VAW": {weight: 0.025, description: "Materials"},
-	"VNQ": {weight: 0.027, description: "Real Estate"},
-	"VPU": {weight: 0.028, description: "Utilities"},
+var sectorEtfs = map[string]string{
+	"VOX": "Communication Services",
+	"VCR": "Consumer Discretionary",
+	"VDC": "Consumer Staples",
+	"VDE": "Energy",
+	"VFH": "Financials",
+	"VHT": "Health Care",
+	"VIS": "Industrials",
+	"VGT": "Information Technology",
+	"VAW": "Materials",
+	"VNQ": "Real Estate",
+	"VPU": "Utilities",
 }
+
+const snp500ETF = "VOO"
 
 func main() {
 	amount := flag.Int("amount", 10000, "the maount of money to invest")
 	n := flag.Int("n", 3, "the top stocks in each sector")
 	flag.Parse()
 	cli := vanguard.NewClient()
+
+	voo, err := cli.GetEtf(snp500ETF)
+	if err != nil {
+		log.Fatalln("failed getting S&P500 etf", err)
+	}
+	di, err := voo.GetDiversificationInfo()
+	if err != nil {
+		log.Fatalln("failed getting diversification info for VOO", err)
+	}
+
 	count := 1
 	toInvest := 0.0
-	for symbol, details := range sectors {
+	for symbol, description := range sectorEtfs {
+		sectorToInvest := 0
 		etf, err := cli.GetEtf(symbol)
 		if err != nil {
 			log.Fatalln("failed getting etf", err)
@@ -47,36 +60,59 @@ func main() {
 		}
 		topHoldings, marketSize := getHoldings(holdings, *n)
 
-		for symbol, value := range topHoldings {
-			holding := math.Round(details.weight * (value / marketSize) * float64(*amount))
+		weight := di.Sectors[description].BenchmarkWeight
+		for _, h := range topHoldings {
+			holding := math.Round(weight * (h.value / marketSize) * float64(*amount))
 			toInvest += holding
-			fmt.Printf("%d. (%v) Buy %v %f$\n", count, details.description, symbol, holding)
+			fmt.Printf("%d. (%v) Buy %v %f$\n", count, description, h.symbol, holding)
 			count++
+			sectorToInvest += int(holding)
 		}
+		defer fmt.Printf("Total invested in %s: %d (weight: %f%% - expected %d) \n", description, sectorToInvest, 100.0*weight, int64(weight*float64(*amount)))
 	}
-	fmt.Println("Total:", toInvest)
+	defer fmt.Println("Total:", toInvest)
 }
 
-func getHoldings(holdings []vanguard.Holding, n int) (map[string]float64, float64) {
-	result := make(map[string]float64, n)
+type holding struct {
+	symbol string
+	value  float64
+}
+
+type holdings []holding
+
+func (h holdings) Len() int           { return len(h) }
+func (h holdings) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h holdings) Less(i, j int) bool { return h[i].value > h[j].value }
+
+func getHoldings(holdings []vanguard.Holding, n int) (holdings, float64) {
+	holdingMap := make(map[string]float64, n)
 	marketSize := 0
 	for _, h := range holdings {
 		if h.Symbol == "" {
 			continue
 		}
-		_, ok := result[h.Symbol]
-		if len(result) == n {
+		_, ok := holdingMap[h.Symbol]
+		if len(holdingMap) == n {
 			if !ok {
-				return result, float64(marketSize)
+				return toOrderedSlice(holdingMap), float64(marketSize)
 			}
 		}
 		marketSize += h.MarketValue
 		if ok {
-			result[h.Symbol] += float64(h.MarketValue)
+			holdingMap[h.Symbol] += float64(h.MarketValue)
 			continue
 		}
 
-		result[h.Symbol] = float64(h.MarketValue)
+		holdingMap[h.Symbol] = float64(h.MarketValue)
 	}
 	return nil, 0
+}
+
+func toOrderedSlice(h map[string]float64) holdings {
+	result := make(holdings, 0, len(h))
+	for k, v := range h {
+		result = append(result, holding{symbol: k, value: v})
+	}
+	sort.Sort(result)
+	return result
 }
